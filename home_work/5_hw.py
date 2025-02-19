edbbc37c9944ee017580e855bf516e50ef0c440f
testirovsik17 <endmitrenko17@gmail.com>
2025-02-19 09:52:28 +0300

    lesson 5 homework

---
+++ home_work/5_hw.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com")
+
+username = driver.find_element(By.CSS_SELECTOR, '#user-name')
+password = driver.find_element(By.CSS_SELECTOR, '#password')
+submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
+
+if input and password and submit:
+    print ('Элементы найдены')
